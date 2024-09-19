a1fc8821ae2a345c548769b7db2f546ced6ee694
pchevallier <philippe@chevallier.net>
2024-09-19 13:17:05 -0400

    added ici premiere and ici musique classique

--- default.py
+++ default.py
@@ -13,9 +13,13 @@ def set_file_constant(file):
 MONITOR = xbmc.Monitor()
 PLAYER = xbmc.Player()
 ADDON_HANDLE = int(sys.argv[1])
-ID = "plugin.audio.ici-musique"
 ICON = set_file_constant("icon.png")
 FANART = set_file_constant("fanart.jpg")
+
+radio_stations = [{'name':'ICI Musique','title':'Montreal','stream':'plugin://plugin.audio.ici-musique/?mode=stream&url=https%3A%2F%2Frcavliveaudio.akamaized.net%2Fhls%2Flive%2F2006979%2FM-7QMTL0_MTL%2Fadaptive_192%2Fchunklist_ao.m3u8&title=ICI+Musique+-+Montreal&key=2&location=montreal'},
+                  {'name':'ICI Premiere','title':'Montreal','stream':'plugin://plugin.audio.ici-musique/?mode=stream&url=https%3A%2F%2Frcavliveaudio.akamaized.net%2Fhls%2Flive%2F2006635%2FP-2QMTL0_MTL%2Fadaptive_192%2Fchunklist_ao.m3u8&title=ICI+Musique+-+Montreal&key=2&location=montreal'},
+                  {'name':'ICI Musique Classique','title':'Montreal','stream':'plugin://plugin.audio.ici-musique/?mode=stream&url=https%3A%2F%2Frcavliveaudio.akamaized.net%2Fhls%2Flive%2F2007000%2FMUSE%2Fadaptive_192%2Fchunklist_ao.m3u8&title=ICI+Musique+-+Montreal&key=2&location=montreal'}]
+
 
 PROGRAM = None
 PROGRAM_SCHEDULE = None
@@ -23,16 +27,27 @@ TRACK = None
 PLAYLOG = None
 KEY = None
 LOCATION = None
+BASE_URL = sys.argv[0]
+ID = BASE_URL.split('://')[1].split('/')[0]
+
+
+def build_url(query):
+    xbmc.log(level=xbmc.LOGINFO, msg=f"{ID}: build_url: {BASE_URL} {query}")
+    return BASE_URL + "?" + urlparse.urlencode(query)
 
 def list_streams(stn):
     xbmc.log(level=xbmc.LOGINFO, msg=f"{ID}: list_streams: {stn}")                                                                                                                                                                                                                   
-    url='plugin://plugin.audio.ici-musique/?mode=stream&url=https%3A%2F%2Frcavliveaudio.akamaized.net%2Fhls%2Flive%2F2006979%2FM-7QMTL0_MTL%2Fadaptive_192%2Fchunklist_ao.m3u8&title=ICI+Musique+-+Montreal&key=2&location=montreal'              
-                                                                                                                                                                                                                                          
-    station_and_title = "ICI Musique - Montreal"                                                                                                                                                                                
-    li = xbmcgui.ListItem(station_and_title)                                                                                                                                                                                    
-    li.setProperty("IsPlayable", "true")                                                                                                                                                               
-    li.setArt({"icon": ICON, "fanart": FANART})                                                                                                                                                        
-    xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE, url=url, listitem=li)  
+
+    for station in radio_stations:
+        if station['name'] == stn:
+            url = station['stream']                                                                                                                                                                                                                                  
+            station_and_title = station['name']+' '+ station['title']   
+            xbmc.log(level=xbmc.LOGINFO, msg=f"{ID}: list_streams: {station_and_title}")                                                                                                                                                                                                                   
+            xbmc.log(level=xbmc.LOGINFO, msg=f"{ID}: list_streams: {url}")
+            li = xbmcgui.ListItem(station_and_title)                                                                                                                                                                                    
+            li.setProperty("IsPlayable", "true")                                                                                                                                                               
+            li.setArt({"icon": ICON, "fanart": FANART})                                                                                                                                                        
+            xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE, url=url, listitem=li)  
     xbmcplugin.endOfDirectory(ADDON_HANDLE)
 
 def update_play_item():
@@ -44,7 +59,7 @@ def update_play_item():
     try:
         play_item = PLAYER.getPlayingItem()
         tag = PLAYER.getMusicInfoTag()
-        #xbmc.log(level=xbmc.LOGINFO, msg=f"{ID}::update_play_item(): {tag.getTitle()}")
+        xbmc.log(level=xbmc.LOGINFO, msg=f"{ID}::update_play_item(): {tag.getTitle()}")
     except RuntimeError:
         xbmc.log(level=xbmc.LOGINFO, msg=f"{ID}::update_play_item(): creating new ListItem")
         play_item = xbmcgui.ListItem()
@@ -73,14 +88,13 @@ def update_play_item():
     except RuntimeError:
         return play_item
 
-
-
 def chill(length):
     MONITOR.waitForAbort(length)
     if MONITOR.abortRequested():
         PLAYER.stop()
 
 def initialize(url):
+    xbmc.log(level=xbmc.LOGINFO, msg=f"{ID}: url: {url}")
     play_item = update_play_item()
     play_item.setPath(url)
     play_item.setProperty('IsPlayable', 'true')
@@ -98,19 +112,21 @@ def initialize(url):
 def play_stream(url):
     initialize(url)
     while not MONITOR.abortRequested():
-        #update_play_item()
+        update_play_item()
         chill(5)
         if not PLAYER.isPlaying():
             sys.exit(0)
+    
 
 def list_stations():                                                                                                                                                                                   
-    url = 'plugin://plugin.audio.ici-musique/?mode=folder&foldername=Ici+Musique'                                                                                                                         
-    station = 'ICI Musique'                                                                                                                                                                            
-    li = xbmcgui.ListItem(station)                                                                                                                                                                     
-    xbmcplugin.addDirectoryItem(                                               
-        handle=ADDON_HANDLE, url=url, listitem=li, isFolder=True               
-    )                                                                                                                                                        
-    xbmcplugin.endOfDirectory(ADDON_HANDLE)                                             
+    for station in radio_stations:
+        url = build_url({"mode": "folder", "foldername": station['name']})
+        xbmc.log(level=xbmc.LOGINFO, msg=f"list_station: {url} {station['name']}")                                                                                                                        
+        li = xbmcgui.ListItem(station['name'])                                                                                                                                                                     
+        xbmcplugin.addDirectoryItem(                                               
+            handle=ADDON_HANDLE, url=url, listitem=li, isFolder=True               
+        )                                                                                                                                                        
+    xbmcplugin.endOfDirectory(ADDON_HANDLE)  
 
 def main():
 
